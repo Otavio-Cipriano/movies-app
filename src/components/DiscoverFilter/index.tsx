827dbdf477a8161cf9babf30121fb7a85c831840
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-08-30 15:37:56 -0300

    add hover to moviecard

---
+++ src/components/DiscoverFilter/index.tsx
@@ -0,0 +1,7 @@
+export default function DiscoverFilter() {
+  return (
+    <div className="discover__filter">
+
+    </div>
+  )
+}
--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Movie from '../../interfaces/movie'
 
-const image_path = 'https://image.tmdb.org/t/p/original'
+const image_path = 'https://image.tmdb.org/t/p/w500'
 
 interface MovieCard{
     movie: Movie,
 }
 
 export default function MovieCard({movie}:MovieCard) {
+
   return (
     <div className='movie-card'>
       <img src={`${image_path}${movie.poster_path}`} alt={movie.title}/>
