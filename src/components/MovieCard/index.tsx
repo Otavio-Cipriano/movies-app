3717b9d788826af8906c39751e3b06fdadca4a27
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-08-30 00:13:21 -0300

    add path_cover to moviecard

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Movie from '../../interfaces/movie'
 
+const image_path = 'https://image.tmdb.org/t/p/original'
+
 interface MovieCard{
     movie: Movie,
 }
 
 export default function MovieCard({movie}:MovieCard) {
   return (
-    <div className='movie-card'>{movie.title}</div>
+    <div className='movie-card'>
+      <img src={`${image_path}${movie.poster_path}`} alt={movie.title}/>
+      <p>{movie.title}</p>
+    </div>
   )
 }
