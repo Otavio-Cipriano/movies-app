c3034e662e0d17a0a2b417bdb3aebc95269d8adf
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-09-02 10:59:02 -0300

    created discoverFilter and added rate in movieCard

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Movie from '../../interfaces/movie'
 
+import {FaStar} from 'react-icons/fa'
+
 const image_path = 'https://image.tmdb.org/t/p/w500'
 
 interface MovieCard{
@@ -11,6 +13,7 @@ export default function MovieCard({movie}:MovieCard) {
 
   return (
     <div className='movie-card'>
+      <span><FaStar className='star'/>{movie.vote_average}</span>
       <img src={`${image_path}${movie.poster_path}`} alt={movie.title}/>
       <p>{movie.title}</p>
     </div>
---
+++ src/interfaces/discoverFilter.ts
@@ -0,0 +1,24 @@
+enum SortBy{
+    popularityAsc ='popularity.asc', 
+    popularityDesc = 'popularity.desc', 
+    releaseDateAsc = 'release_date.asc', 
+    releaseDateDesc = 'release_date.desc', 
+    revenueAsc = 'revenue.asc', 
+    revenueDesc = 'revenue.desc', 
+    primaryReleaseDateAsc = 'primary_release_date.asc', 
+    primaryReleaseDateDesc = 'primary_release_date.desc', 
+    originalTitleAsc = 'original_title.asc', 
+    originalTitleDesc ='original_title.desc', 
+    voteAverageAsc = 'vote_average.asc', 
+    voteAverageDesc = 'vote_average.desc', 
+    voteCountAsc = 'vote_count.asc', 
+    voteCountDesc = 'vote_count.desc'
+}
+
+interface DiscoverFilter{
+    sort_by: SortBy | SortBy.popularityAsc,
+    with_genres?: string,
+    without_genre?: string
+}
+
+export default DiscoverFilter;
